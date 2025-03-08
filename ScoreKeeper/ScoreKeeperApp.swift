675c79fc029d8b71c0602b05a923c4cadf4067bd
Chris Bille <cbille1@icloud.com>
2025-03-08 10:22:39 -0500

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Christopher Bille on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
